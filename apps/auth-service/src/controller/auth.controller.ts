8c57392e2395d6f98c4fc6bfccdaf8b06564db4e
RutvikMakvana <rutvik.makvana@dignizant.com>
2025-10-28 15:56:11 +0530

    add redis connection configuration

--- apps/auth-service/src/controller/auth.controller.ts
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -28,7 +28,7 @@ export const userRegistration = async (
 
     await checkOtpRestrictions(email, next);
     await trackOtpRequests(email, next);
-    await sendOtp(email, name, "user-activation-mail");
+    await sendOtp(name, email, "user-activation-mail");
     res.status(200).json({
       message: "OTP sent to email. Please verify your account.",
     });
